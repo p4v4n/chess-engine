1340eb83cb9ff857fbd7855a28b28a0c02b67b80
p4v4n <pavan.barcelona@gmail.com>
2017-07-12 10:29:25 +0530

    Add initial-board as a vector

---
+++ src/chess_engine_clj/engine.clj
@@ -0,0 +1,13 @@
+(ns chess-clj.engine
+    (:require [clojure.string :as string]))
+
+(defn initial-board []
+	[\r \n \b \q \k \b \n \r
+	 \p \p \p \p \p \p \p \p
+	 \- \- \- \- \- \- \- \-
+	 \- \- \- \- \- \- \- \-
+	 \- \- \- \- \- \- \- \-
+	 \- \- \- \- \- \- \- \-
+	 \P \P \P \P \P \P \P \P
+	 \R \N \B \Q \K \B \N \R])
+
