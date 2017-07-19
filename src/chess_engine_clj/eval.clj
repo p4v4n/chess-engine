22d8d91960535f4eb03eb802532381b816dba143
p4v4n <pavan.barcelona@gmail.com>
2017-07-19 15:01:26 +0530

    Add fn to assign a number to a given board position

--- src/chess_engine_clj/eval.clj
+++ src/chess_engine_clj/eval.clj
@@ -3,6 +3,9 @@
 ;;Evaluation for black-side
 
 (def piece-value {\p 1 \n 3 \b 3 \r 5 \q 9 \k 1000
-	              \P -1 \N -3 \B -3 \R -5 \Q -9 \K -1000})
+	              \P -1 \N -3 \B -3 \R -5 \Q -9 \K -1000 \- 0})
 
-
+(defn eval-position [board-vec]
+  (->> (flatten board-vec)
+       (map piece-value)
+       (reduce +)))
