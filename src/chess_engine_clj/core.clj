9f304884e26832654125c76f97810021c27b26fa
p4v4n <pavan.barcelona@gmail.com>
2017-07-21 11:41:01 +0530

    Divide game-play code into 2 seperate functions.   -Add a player-to-move-fn to map color to corresponding function.

--- src/chess_engine_clj/core.clj
+++ src/chess_engine_clj/core.clj
@@ -6,16 +6,23 @@
               [chess-engine-clj.search :as search])
   (:gen-class))
 
-(defn game-play []
-    (println "Your Move: ")
+(defn white-move []
+  (println "Your Move: ")
     (let [user-move (string/trim (read-line))]
-        (board/make-move user-move))
-    (Thread/sleep 2000)
+        (board/make-move user-move)))
+
+(defn black-move []
     (let [current-board (:board @board/board-state)
           valid-move-list (movegen/engine-valid-move-list current-board)
           engine-move (search/pick-best-move current-board valid-move-list eval/eval-position)]
+        (Thread/sleep 1000)
         (println (str "My Move: " engine-move "\n"))
         (board/make-move engine-move)))
+
+(def player-to-move-fn {"white" white-move "black" black-move})
+
+(defn game-play []
+    ((player-to-move-fn (:turn @board/board-state))))
 
 (defn -main []
     (while true
